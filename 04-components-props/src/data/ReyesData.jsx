2ac097b46da7d3442ca7a6befda12c1501f008d3
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-03-23 17:37:34 -0500

    Creating a local DB characters

---
+++ 04-components-props/src/data/ReyesData.jsx
@@ -0,0 +1,33 @@
+export const reyes=[
+    {
+      nombre:"Atanagildo",
+      color:"darkolivegreen",
+      precio:178,
+      url: "https://www.html6.es/img/rey_atanagildo.png"
+    },{
+      nombre:"Ervigio",
+      color:"crimson",
+      precio:169,
+      url: "https://www.html6.es/img/rey_ervigio.png"
+    },{
+      nombre:"Ataúlfo",
+      color:"peru",
+      precio:81,
+      url: "https://www.html6.es/img/rey_ataulfo.png"
+    },{
+      nombre:"Leogivildo",
+      color:"darkmagenta",
+      precio:126,
+      url: "https://www.html6.es/img/rey_leogivildo.png"
+    },{
+      nombre:"Recesvinto",
+      color:"royalblue",
+      precio:141,
+      url: "https://www.html6.es/img/rey_recesvinto.png"
+    },{
+      nombre:"Sisebuto",
+      color:"teal",
+      precio:69,
+      url: "https://www.html6.es/img/rey_sisebuto.png"
+    }
+  ]
