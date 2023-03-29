845f6915dc8a96be9bece885614008bb1453125c
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-03-29 13:12:07 -0500

    Adding challenge description to main component

--- 06-useefect-fetch-spread/src/App.js
+++ 06-useefect-fetch-spread/src/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <>
       <h1>UseEffect, Fetch and Spread Operator</h1>
-      <ChallengeJokeComponent />
+      <div className="subcontainer">
+        <h3 className="text-description">1) Consume the indicated Public API and represent the data through a new component, 
+          control the requests through the Hook: useEffect.<br/>Assign a state to the button to make a new request and change the 
+          information represented in the component.
+        </h3>
+        <ChallengeJokeComponent />
+      </div>
+      
     </>
   );
 }
